5a31094c864882dee175068b908f577e64ade770
JairoPilliza <jairopz2000@gmail.com>
2022-06-08 12:08:43 -0500

    puesto nombre table ATS

--- src/component/ATS/TableATS.jsx
+++ src/component/ATS/TableATS.jsx
@@ -47,11 +47,12 @@ const TableATS = () => {
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell>Dessert (100g serving)</TableCell>
-                            <TableCell align="right">Calories</TableCell>
-                            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                            <TableCell>N°</TableCell>
+                            <TableCell align="right">Nombre del Archivo</TableCell>
+                            <TableCell align="right">Código</TableCell>
+                            <TableCell align="right">Sucursal</TableCell>
+                            <TableCell align="right">Período</TableCell>
+                            <TableCell align="right">Tipo</TableCell>
                             <TableCell align="right">Acciones</TableCell>
                         </TableRow>
                     </TableHead>
@@ -70,7 +71,9 @@ const TableATS = () => {
                                 <TableCell align="right">{row.fat}</TableCell>
                                 <TableCell align="right">{row.carbs}</TableCell>
                                 <TableCell align="right">{row.protein}</TableCell>
-                                <TableCell align="right"><Button variant="contained" startIcon={<LocalPrintshopIcon />}></Button>
+                                <TableCell align="right">{row.protein}</TableCell>
+
+                                <TableCell align="right">
                                     <Button variant="contained" startIcon={<MailOutlineIcon />}></Button></TableCell>
                             </TableRow>
                         ))}
