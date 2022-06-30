3b9aec6143904204a3489d695ab37e8102a88128
JairoPilliza <jairopz2000@gmail.com>
2022-06-30 17:14:10 -0500

    .

--- src/component/LiquidacionCompra/LiquidacionCompra.jsx
+++ src/component/LiquidacionCompra/LiquidacionCompra.jsx
@@ -457,8 +457,6 @@ const LiquidacionCompra = () => {
                                     <Grid item lg={12} md={12} sm={12} xs={12} >
                                         <TextField id="valorTotal" name="valorTotal" label="Valor Total:" variant="outlined"  {...register("valorTotal")} style={{ width: "100%" }} />
                                     </Grid>
-
-
                                 </Grid>
                             </Grid>
                             <br></br>
